ffbe759f0b3b90a5406c06479ef6a8b7bf319fb2
besv19ab <besv19ab@student.cbs.dk>
2020-01-25 00:56:15 +0100

    forside færdig samt menu knapper commit af Betina

--- GoSave.js
+++ GoSave.js
@@ -1,2 +1,7 @@
 // Dette er vores Javascript
-halløjsa
+// Denne funktion åbner en ny side i samme vindue ved tryk på button
+function openPage() {
+    var myWindow = window.open("", "_self");
+    myWindow.document.write("<p>I replaced the current window.</p>");
+}
+
