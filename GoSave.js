e7cd1470b060df8703aa4cd29a612b831c444696
besv19ab <besv19ab@student.cbs.dk>
2020-01-25 01:22:56 +0100

    blot lille kommentar commit af Betina

--- GoSave.js
+++ GoSave.js
@@ -1,5 +1,5 @@
 // Dette er vores Javascript
-// Denne funktion åbner en ny side i samme vindue ved tryk på button
+// Denne funktion åbner en ny side i samme vindue ved tryk på button. en mulig løsning til vores buttons
 function openPage() {
     var myWindow = window.open("", "_self");
     myWindow.document.write("<p>I replaced the current window.</p>");
