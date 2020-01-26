b6a68780f303a3d7f88085f41de6f64d7de7975b
besv19ab <besv19ab@student.cbs.dk>
2020-01-26 23:17:56 +0100

    oprettet procent siden. commit af Betina

--- GoSave.js
+++ GoSave.js
@@ -1,7 +1,3 @@
 // Dette er vores Javascript
 // Denne funktion åbner en ny side i samme vindue ved tryk på button. en mulig løsning til vores buttons
-function openPage() {
-    var myWindow = window.open("", "_self");
-    myWindow.document.write("<p>I replaced the current window.</p>");
-}
 
