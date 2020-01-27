b1f810549676c7d6b134ba96f067fff7d7e25980
besv19ab <besv19ab@student.cbs.dk>
2020-01-27 20:14:42 +0100

    Procent funktion implementeret samt billeder sat ind. commit af Betina

--- GoSaveBetina.js
+++ GoSaveBetina.js
@@ -1,26 +1,29 @@
 // Dette er vores Javascript
 // Denne funktion åbner en ny side i samme vindue ved tryk på button. en mulig løsning til vores
 
-// Dette er funktionerne til procent
+// BS. Dette er funktionen til procentvis besparelse
 function udregnProcentvisBesparelse() {
-    if (!$('#besparelsepris').is(":visible")) {
-        $('#besparelsepris').show();
-    }
+    document.getElementById("resultDiv").style.display = "block";
 
-    var foer = document.getElementById('foerpris').value;
-    var efter = document.getElementById('nypris').value;
+    let foer = document.getElementById("previousPrice").value;
+    let efter = document.getElementById("newPrice").value;
     if (!isNaN(foer) && !isNaN(efter)) {
-
         if (foer > efter) {
-            var sparet = foer-efter;
-            var procentbesparelse = (sparet/foer)*100;
-
-            document.getElementById('besparelse').innerHTML = procentbesparelse.toFixed(2) + "%";
+            let sparet = foer - efter;
+            let procentbesparelse = (sparet / foer) * 100;
+            document.getElementById("besparelse").innerHTML = procentbesparelse.toFixed(2) + "%";
         }
     }
-
 }
 
+
+
+
+
+
+
+
+/*
 function udregnProcentvisStigning() {
     if (!$('#besparelsepris').is(":visible")) {
         $('#besparelsepris').show();
@@ -59,3 +62,4 @@ function udregnProcentvisStigning() {
             }
         }
 }
+*/
