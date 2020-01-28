335d16f2f0a0cb942a7e422516fdd8ec8e8efb9f
besv19ab <besv19ab@student.cbs.dk>
2020-01-28 14:04:50 +0100

    funktion for procentvisstigning implementeret. commit af Betina

--- GoSaveBetina.js
+++ GoSaveBetina.js
@@ -3,46 +3,37 @@
 
 // BS. Dette er funktionen til procentvis besparelse
 function udregnProcentvisBesparelse() {
-    document.getElementById("resultDiv").style.display = "block";
+    document.getElementById("resultDiv1").style.display = "block";
 
-    let foer = document.getElementById("previousPrice").value;
-    let efter = document.getElementById("newPrice").value;
+    let foer = document.getElementById("previousPrice1").value;
+    let efter = document.getElementById("newPrice1").value;
     if (!isNaN(foer) && !isNaN(efter)) {
+
         if (foer > efter) {
             let sparet = foer - efter;
             let procentbesparelse = (sparet / foer) * 100;
-            document.getElementById("besparelse").innerHTML = procentbesparelse.toFixed(2) + "%";
+            document.getElementById("besparelse1").innerHTML = procentbesparelse.toFixed(2) + "%";
+        }
+    }
+}
+// BS. Dette er funktionen til beregning af procentvis stigning
+function udregnProcentvisStigning() {
+    document.getElementById("resultDiv2").style.display = "block";
+
+    let foer = document.getElementById('previousPrice2').value;
+    let efter = document.getElementById('newPrice2').value;
+    if (!isNaN(foer) && !isNaN(efter)) {
+
+        if (foer < efter) {
+            let sparet = efter - foer;
+            let procentstigning = (sparet / foer) * 100;
+
+            document.getElementById('stigning').innerHTML = procentstigning.toFixed(2) + "%";
         }
     }
 }
 
-
-
-
-
-
-
-
 /*
-function udregnProcentvisStigning() {
-    if (!$('#besparelsepris').is(":visible")) {
-        $('#besparelsepris').show();
-    }
-
-    var foer = document.getElementById('foerpris').value;
-    var efter = document.getElementById('nypris').value;
-    if (!isNaN(foer) && !isNaN(efter)) {
-
-        if (foer < efter) {
-            var sparet = efter-foer;
-            var procentbesparelse = (sparet/foer)*100;
-
-            document.getElementById('besparelse').innerHTML = procentbesparelse.toFixed(2) + "%";
-        }
-    }
-
-
-)
 
     function udregnPrisEfterProcent() {
         if (!$('#besparelsepris').is(":visible")) {
