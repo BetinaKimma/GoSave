3260248cd32d83b6f7da0dcf200a68bd0cfcfa9e
Tayyab Latif <mola19af@student.cbs.dk>
2020-02-11 19:44:35 +0100

    JS opdateret

--- GoSaveTayyab.js
+++ GoSaveTayyab.js
@@ -1,9 +1,13 @@
+let brugernavn = [];
+console.log(brugernavn);
+
+console.log(JSON.parse(localStorage.getItem('brugernavn')));let brugernavn = [];
 
 function brugernavn() {
-    brugernavn.push(brugernavn().value);
+    brugernavn.push("brugernavn"().value);
     localStorage.setItem('brugernavn', brugernavn.value);
-    alert('En ny bruger er logget på');
-    console.log(brugernavn());
+    alert('En bruger er logget på');
+    console.log("brugernavn"());
     localStorage.setItem('brugernavn', JSON.stringify(brugernavn));
 
     // JSON står for JavaScript Object Notation
