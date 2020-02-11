53cec6184863afbad7fe1d7332602d1f36f6b234
HyggenSB <55584411+HyggenSB@users.noreply.github.com>
2020-02-11 20:17:28 +0100

    Opstillet færdig implementeret funktion for MaengdeRabat. commit af Scott. Lavet layout for MaengdeRabat. Commit af Scott.

--- GoSaveScott.js
+++ GoSaveScott.js
@@ -1,27 +1,27 @@
-//Dette er vores JS for TaxFree og Meangderabat
-//Dette er funktionen til beregning af pris efter TaxFree procenten
-function udregnTaxfreeSaving() {
-    document.getElementById(elementid: "resultatDivTax").style.display = "block";
+//Dette er vores JS for TaxFree og Meangderabat. lavet af Scott
 
-    let foer = document.getElementById(elementid: "prisFørTax").value;
-    let taxfree = document.getElementById(elementid "nyPrisTax").value;
+//SB. Dette er funktionen til beregning af pris efter TaxFree procenten
+function udregnTaxfreeSaving() {
+    document.getElementById("resultatDivTax").style.display = "block";
+
+    let foer = document.getElementById("prisFørTax").value;
+    let taxfree = document.getElementById("nyPrisTax").value;
     if (!isNaN(foer) && !isNaN(taxfree)) {
 
             let taxFreePris = foer / 100;
             let taxFreeSaving = taxfree * taxFreePris;
             let taxFreeRabat = foer - taxFreeSaving;
 
-            document.getElementById(elementid: "taxrabat").innerHTML = taxFreeRabat.toFixed(fractionDigits: 2) + "kr";
+            document.getElementById("taxrabat").innerHTML = taxFreeRabat.toFixed(2) + "kr";
         }
-
     }
 
-    //Dette er funktionen til beregning af mængderabat
+    //SB. Dette er funktionen til beregning af mængderabat
 function udregnMaengdeRabat() {
-    document.getElementById(elementid: "resultatDivMaengde").style.diplay = "block";
-    let antal = document.getElementById(elementid: "antalProdukter").value;
-    let foer = document.getElementById(elementid: "stykPrisFørR").value;
-    let maengderabat = document.getElementById(elementid: "antalBetaler").value;
+    document.getElementById("resultatDivMaengde").style.diplay = "block";
+    let antal = document.getElementById("antalProdukter").value;
+    let foer = document.getElementById("stykPrisFørR").value;
+    let maengderabat = document.getElementById("antalBetaler").value;
     if (!isNaN(antal) && !isNaN(foer) && !isNaN(maengderabat)) {
 
         let antalBetaling = maengderabat * foer;
