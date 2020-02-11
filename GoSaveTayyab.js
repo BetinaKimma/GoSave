5fbd596728e3bb1cc5a9a21b3c42d7f5d6bfcaab
Tayyab Latif <mola19af@student.cbs.dk>
2020-02-11 19:31:01 +0100

    JS opdateret

---
+++ GoSaveTayyab.js
@@ -0,0 +1,19 @@
+
+function brugernavn() {
+    brugernavn.push(firstname.value);
+    localStorage.setItem('brugernavn', brugernavn.value);
+    alert('En ny bruger er logget på');
+    console.log(brugernavn());
+    localStorage.setItem('brugernavn', JSON.stringify(brugernavn));
+
+    // JSON står for JavaScript Object Notation
+    // stringify er en metode vi bruger for JSON i Javascript
+}
+
+
+
+
+
+
+
+
